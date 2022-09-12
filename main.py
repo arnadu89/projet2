6485213b2f24ebd1f79b217330b9ff92eedefe42
arnadu89 <arnadu89@gmail.com>
2022-09-12 15:11:25 +0200

    scrap book datas from url

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_base = "https://books.toscrape.com"
+
+
+def get_soup_from_url(url):
+    """Get soup bs4 object from url request"""
+    html_page = requests.get(url)
+
+    if html_page.ok:
+        return BeautifulSoup(html_page.content, "html.parser")
+    else:
+        raise FileNotFoundError
+
+
+def scrap_book_field(book, book_soup, key, css_selector):
+    try:
+        book[key] = book_soup.select_one(css_selector).text
+    except AttributeError:
+        book[key] = ""
+
+
+def scrap_book_data_from_url(book_url):
+    """Scrap information about desired book"""
+    book = dict()
+
+    book_soup = get_soup_from_url(book_url)
+
+    book["product_page_url"] = book_url
+
+    scrap_book_field(book, book_soup, "universal_product_code", ".table tr:nth-child(1) td")
+    scrap_book_field(book, book_soup, "title", ".product_main h1")
+    scrap_book_field(book, book_soup, "price_including_tax", ".table tr:nth-child(4) td")
+    scrap_book_field(book, book_soup, "price_excluding_tax", ".table tr:nth-child(3) td")
+    scrap_book_field(book, book_soup, "number_available", ".table tr:nth-child(6) td")
+    scrap_book_field(book, book_soup, "product_description", "#product_description + p")
+    scrap_book_field(book, book_soup, "category", ".breadcrumb li:nth-child(3) a")
+
+    book["review_rating"] = book_soup.select_one(".star-rating").attrs["class"][1]
+    image_url = book_soup.select_one("#product_gallery img").attrs["src"]
+    image_url = f"{url_base}/{image_url[6:]}"
+    book["image_url"] = image_url
+
+    image_file_name = f"./book_pictures/{book['universal_product_code']}.png"
+
+    return book
