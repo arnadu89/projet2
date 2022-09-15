0731c3380de6b4ecf3c937a32888c63894052e86
arnadu89 <arnadu89@gmail.com>
2022-09-15 10:57:06 +0200

    improve verbose on main file

--- booktoscrape/__main__.py
+++ booktoscrape/__main__.py
@@ -5,12 +5,15 @@ from booktoscrape.extract import create_csv_book_file, add_books_to_csv
 
 
 def main():
+    """Scraping books from all categories of booktoscrape website"""
+    print(f"Scraping books from : {BOOKTOSCRAPE_URL}")
     categories_urls = scrap_categories_urls(BOOKTOSCRAPE_URL)
     for category_url in categories_urls:
-        print(category_url)
-        books = scrap_all_books_from_category(category_url)
-
         category_name = category_url.split("/")[6]
+        print(f"Current scraping category is : {category_name}")
+
+        books = scrap_all_books_from_category(category_url)
+        print(f"Books scraped from this category : {len(books)}")
 
         csv_file_name = f"{CSV_FILES_FOLDER}/books_{category_name}.csv"
         create_csv_book_file(csv_file_name)
